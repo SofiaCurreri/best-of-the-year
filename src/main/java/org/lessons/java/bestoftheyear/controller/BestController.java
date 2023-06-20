1d48a7720ead85375aff1e67ae357670b2a2173c
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-20 17:32:20 +0200

    step 2 completed

--- src/main/java/org/lessons/java/bestoftheyear/controller/BestController.java
+++ src/main/java/org/lessons/java/bestoftheyear/controller/BestController.java
@@ -1,9 +1,14 @@
 package org.lessons.java.bestoftheyear.controller;
 
+import org.lessons.java.bestoftheyear.Movie;
+import org.lessons.java.bestoftheyear.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/") //a quale rotta rispondono i metodi di questo controller
@@ -14,5 +19,49 @@ public class BestController {
         String name = "Sofia";
         model.addAttribute("name", name);
         return "index";
+    }
+
+    private List<Movie> getBestMovies(){
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1, "Labyrinth"));
+        movies.add(new Movie(2, "Aladdin"));
+        movies.add(new Movie(3, "Paprika"));
+        return movies;
+    }
+
+    private List<Song> getBestSongs(){
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1, "Pedro"));
+        songs.add(new Song(2, "My heart will go on"));
+        songs.add(new Song(3, "Can I call you tonight?"));
+        return songs;
+    }
+
+    @GetMapping("/movies")
+    public String getMovies(Model model){
+        List<Movie> moviesList = getBestMovies();
+        String listTitles = "";
+        for (Movie movie: moviesList) {
+            listTitles += movie.getTitle();
+            if (movie.getId() != moviesList.size()){
+                listTitles += ", ";
+            }
+        }
+        model.addAttribute("moviesList", listTitles);
+        return "movies";
+    }
+
+    @GetMapping("/songs")
+    public String getSongs(Model model){
+        List<Song> songsList = getBestSongs();
+        String listTitles = "";
+        for (Song song: songsList) {
+            listTitles += song.getTitle();
+            if (song.getId() != songsList.size()){
+                listTitles += ", ";
+            }
+        }
+        model.addAttribute("songsList", listTitles);
+        return "songs";
     }
 }
